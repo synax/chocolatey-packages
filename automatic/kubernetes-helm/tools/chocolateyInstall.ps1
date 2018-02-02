f0da76d9b9a9194648bf2ae7bef5a3fb863abf23
Stefan Henseler <stefan.henseler@synax.ch>
2018-02-02 10:56:04 +0100

    Updates Template

---
+++ automatic/kubernetes-helm/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+$packageName    = '{{PackageName}}'
+$packageVersion = '{{PackageVersion}}'
+$url            = '{{DownloadUrl}}'
+$url64          = '{{DownloadUrlx64}}'
+$checksum       = '{{Checksum}}'
+$checksum64     = '{{Checksumx64}}'
+$checksumType   = 'sha256'
+$checksumType64 = 'sha256'
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$tempDir    = "$toolsDir\temp"
+
+
+# Download and unzip into a temp folder
+Install-ChocolateyZipPackage -PackageName "$packageName" -UnzipLocation "$tempDir" -Url "$url" -Url64 "$url64" -Checksum "$checksum" -ChecksumType "$checksumType" -Checksum64 "$checksum64" -ChecksumType64 "$checksumType64"
+
+# Untar into tools folder
+Get-ChocolateyUnzip -PackageName "$packageName" -FileFullPath "$tempDir" -Destination "$toolsDir"
+
+# Remove temp folder
+Remove-Item "$tempDir\*" -recurse
+Remove-Item "$tempDir"
