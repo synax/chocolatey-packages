b28cf3e14d62cdfb93588c231f2449a4a246cbb3
Stefan Henseler <stefan.henseler@synax.ch>
2018-03-09 22:51:02 +0100

    Fixes update script

--- automatic/kubernetes-helm/update.ps1
+++ automatic/kubernetes-helm/update.ps1
@@ -16,16 +16,15 @@ function global:au_GetLatest {
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
     $download_page = Invoke-WebRequest -Uri $releases 
 
-    #tidy-5.1.25-win64.zip
-    $re  = "helm-.+windows-amd(32|64).tar.gz"
-    $url = $download_page.links | Where-Object href -match $re | Select-Object -First 2 -expand href
+    $re  = "helm-.+windows-amd(64).zip"
+    $url = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
 
-    $version = ($url[0] -split '-' | Select-Object -Last 3 | Select-Object -First 1).TrimStart('v')
-    $url64 = $url[0]
-    $checksumByte = Invoke-WebRequest -Uri $url[1] | Select-Object -ExpandProperty Content
-    $checksum64 = ([System.Text.Encoding]::UTF8.GetString($checksumByte)).Trim()
+    $version = ($url -split '-' | Select-Object -Last 3 | Select-Object -First 1).TrimStart('v')
 
-    $Latest = @{ URL64 = $url64; Checksum64 = $checksum64; Version = $version }
+    $content = Invoke-WebRequest $url | Select-Object -ExpandProperty Content
+    $checksum = Get-FileHash -Algorithm SHA256 -InputStream ([System.IO.MemoryStream]::New($Content))
+
+    $Latest = @{ URL64 = $url; Checksum64 = $checksum.Hash.ToLower(); Version = $version }
 
     return $Latest
 } 
