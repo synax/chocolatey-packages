9c3b23b5ae487f9b617a952cb2f08d747f2aa635
Stefan Henseler <stefan.henseler@synax.ch>
2018-03-14 13:12:51 +0100

    Uses splatting for install cmdlet

--- automatic/kubernetes-helm/tools/chocolateyInstall.ps1
+++ automatic/kubernetes-helm/tools/chocolateyInstall.ps1
@@ -1,15 +1,16 @@
-﻿$packageName    = 'helm'
-$packageVersion = '2.8.1'
-$url64          = 'https://storage.googleapis.com/kubernetes-helm/helm-v2.8.1-windows-amd64.zip'
-$checksum64     = '8fbc9e425eb5f806b7f562a03ed03e12fad702a8ce7fc52b7b3409522f33c4f9'
-$checksumType64 = 'sha256'
-
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$tempDir    = "$toolsDir\temp"
+﻿$packageArgs = @{
+    $packageName    = 'helm'
+    #$packageVersion = '2.8.1'
+    $url64          = 'https://storage.googleapis.com/kubernetes-helm/helm-v2.8.1-windows-amd64.zip'
+    $checksum64     = '8fbc9e425eb5f806b7f562a03ed03e12fad702a8ce7fc52b7b3409522f33c4f9'
+    $checksumType64 = 'sha256'    
+    $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+    $tempDir    = "$toolsDir\temp"
+}
 
 
 # Download and unzip into a temp folder
-Install-ChocolateyZipPackage -PackageName "$packageName" -UnzipLocation "$tempDir" -Url "$url" -Url64 "$url64" -Checksum "$checksum" -ChecksumType "$checksumType" -Checksum64 "$checksum64" -ChecksumType64 "$checksumType64"
+Install-ChocolateyZipPackage @packageArgs
 
 # Untar into tools folder
 Get-ChocolateyUnzip -PackageName "$packageName" -FileFullPath "$tempDir" -Destination "$toolsDir"
