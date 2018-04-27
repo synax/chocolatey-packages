9587fc52f7a0f3bdccd21c3991d608f67401bd67
Stefan Henseler <stefan.henseler@synax.ch>
2018-04-27 12:02:43 +0200

    Changes Regex to support zip and tar.gz archives

--- automatic/kubernetes-helm/update.ps1
+++ automatic/kubernetes-helm/update.ps1
@@ -16,8 +16,8 @@ function global:au_GetLatest {
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
     $download_page = Invoke-WebRequest -Uri $releases 
 
-    $re  = "helm-.+windows-amd(64).tar.gz"
-    $url = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
+    $re  = "helm-.+windows-amd64.(tar.gz|zip)"
+    $url = $download_page.links | Where-Object href -match $re, | Select-Object -First 1 -expand href
 
     $version = ($url -split '-' | Select-Object -Last 3 | Select-Object -First 1).TrimStart('v')
 
