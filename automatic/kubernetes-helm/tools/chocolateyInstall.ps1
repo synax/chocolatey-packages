d9b0b738bc61d6453503414225ccd3d9bc02a4c2
Stefan Henseler <stefan.henseler@synax.ch>
2018-04-28 13:46:22 +0200

    Updates Script

--- automatic/kubernetes-helm/tools/chocolateyInstall.ps1
+++ automatic/kubernetes-helm/tools/chocolateyInstall.ps1
@@ -1,18 +1,19 @@
-﻿$packageArgs = @{
-    $packageName    = 'helm'
-    $packageVersion = '2.8.0'
-    $url64          = 'https://storage.googleapis.com/kubernetes-helm/helm-v2.8.0-windows-amd64.zip'
-    $checksum64     = '6b8fbe206511ab790a4f5be690e42fe418dd95fc6a69714ed03cfa70c488827d'
-    $checksumType64 = 'sha256'
-    $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-    $tempDir        = "$toolsDir\temp"
+﻿
+
+$packageName = 'kubernetes-helm'
+$toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$tempDir = "$toolsDir\temp"
+
+$packageArgs = @{
+    PackageName    = $packageName
+    Url64bit       = 'https://storage.googleapis.com/kubernetes-helm/helm-v2.8.0-windows-amd64.zip'
+    Checksum64     = '6b8fbe206511ab790a4f5be690e42fe418dd95fc6a69714ed03cfa70c488827d'
+    ChecksumType64 = 'sha256'
+    UnzipLocation  = $tempDir
 }
 
 # Download and unzip into a temp folder
-Install-ChocolateyZipPackage  @packageArgs
-
-# Untar into tools folder
-Get-ChocolateyUnzip -PackageName "$packageName" -FileFullPath "$tempDir" -Destination "$toolsDir"
+Install-ChocolateyZipPackage @packageArgs
 
 # Remove temp folder
 Remove-Item "$tempDir\*" -recurse
--- automatic/kubernetes-helm/update.ps1
+++ automatic/kubernetes-helm/update.ps1
@@ -7,7 +7,6 @@ function global:au_SearchReplace {
         'tools\chocolateyInstall.ps1' = @{
             "(^\s*[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
             "(^\s*[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-            "(^\s*[$]packageVersion\s*=\s*)('.*')" = "`$1'$($Latest.Version)'"
         }
      }
 }
