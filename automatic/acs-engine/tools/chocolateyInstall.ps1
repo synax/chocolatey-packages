af7bb461427f0b58bcfd57ebe260182a2fa8e659
Stefan Henseler <stefan.henseler@synax.ch>
2018-05-01 20:59:55 +0200

    adds acs-engine package

---
+++ automatic/acs-engine/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿
+$packageName = 'acs-engine'
+$toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$tempDir = "$toolsDir\temp"
+
+$packageArgs = @{
+    PackageName    = $packageName
+    Url64bit       = 'https://github.com/Azure/acs-engine/releases/download/v0.16.1/acs-engine-v0.16.1-windows-amd64.zip'
+    Checksum64     = 'f4c111d44010a0c5c6a3c1dd097320477439985b9acec47ec0b616252796c8e8'
+    ChecksumType64 = 'sha256'
+    UnzipLocation  = $toolsDir
+}
+
+# Download and unzip into a temp folder
+Install-ChocolateyZipPackage @packageArgs
