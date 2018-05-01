335452ad29a3d9c0aeccba919ed573dbab30bdca
Stefan Henseler <stefan.henseler@synax.ch>
2018-05-01 11:34:08 +0200

    Fixes install script

--- automatic/kubernetes-helm/tools/chocolateyInstall.ps1
+++ automatic/kubernetes-helm/tools/chocolateyInstall.ps1
@@ -8,12 +8,8 @@ $packageArgs = @{
     Url64bit       = 'https://storage.googleapis.com/kubernetes-helm/helm-v2.9.0-windows-amd64.zip'
     Checksum64     = '6b8fbe206511ab790a4f5be690e42fe418dd95fc6a69714ed03cfa70c488827d'
     ChecksumType64 = 'sha256'
-    UnzipLocation  = $tempDir
+    UnzipLocation  = $toolsDir
 }
 
 # Download and unzip into a temp folder
 Install-ChocolateyZipPackage @packageArgs
-
-# Remove temp folder
-Remove-Item "$tempDir\*" -recurse
-Remove-Item "$tempDir"
