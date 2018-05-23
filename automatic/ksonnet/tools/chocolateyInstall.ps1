abeae8b7ce2e7bef32b24898605e1fccc5c6b2ee
Stefan Henseler <stefan.henseler@synax.ch>
2018-05-23 09:44:28 +0200

    adds package for ksonnet

---
+++ automatic/ksonnet/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿
+$packageName = 'kubernetes-helm'
+$toolsDir =  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$tempDir = "$toolsDir\temp"
+
+$packageArgs = @{
+    PackageName    = $packageName
+    Url64bit       = 'https://storage.googleapis.com/kubernetes-helm/helm-v2.9.0-windows-amd64.zip'
+    Checksum64     = '6b8fbe206511ab790a4f5be690e42fe418dd95fc6a69714ed03cfa70c488827d'
+    ChecksumType64 = 'sha256'
+    UnzipLocation  = $toolsDir
+}
+
+# Download and unzip into a temp folder
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/ksonnet/update.ps1
@@ -0,0 +1,33 @@
+import-module au -Force
+
+$releases = 'https://github.com/ksonnet/ksonnet/releases'
+$github = 'https://github.com'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^\s*Url64bit\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^\s*Checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+    $download_page = Invoke-WebRequest -Uri $releases 
+
+    $re  = "ks_.+windows_amd64.(tar.gz|zip)"
+    $url = $github + $($download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href)
+
+    $version = ($url -split '_' | Select-Object -Last 3 | Select-Object -First 1).TrimStart('v')
+
+    $content = Invoke-WebRequest $url | Select-Object -ExpandProperty Content
+    $checksum = Get-FileHash -Algorithm SHA256 -InputStream ([System.IO.MemoryStream]::New($Content))
+    $checksum64 = $checksum.Hash.ToLower()
+
+    $Latest = @{ URL64 = "$url"; Version = $version; Checksum64 = $checksum64}
+
+    return $Latest
+} 
+
+Update-Package -ChecksumFor none -Verbose
