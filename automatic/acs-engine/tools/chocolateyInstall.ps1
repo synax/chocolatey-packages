e9c66c38a76e041164632c49f034b93c0b57c895
synax <stefan.henseler@synax.ch>
2018-11-16 01:29:00 +0000

    AU: 1 updated - acs-engine [skip ci]

--- automatic/acs-engine/tools/chocolateyInstall.ps1
+++ automatic/acs-engine/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $tempDir = "$toolsDir\temp"
 
 $packageArgs = @{
     PackageName    = $packageName
-    Url64bit       = 'https://github.com/Azure/acs-engine/releases/download/v0.25.1/acs-engine-v0.25.1-windows-amd64.zip'
-    Checksum64     = '366e98e7e862c371037aadba4dc6e9dd7dc23e9b221aa1666a8bb2c840c5d4d4'
+    Url64bit       = 'https://github.com/Azure/acs-engine/releases/download/v0.25.2/acs-engine-v0.25.2-windows-amd64.zip'
+    Checksum64     = 'b6db9569e701c983961fc4757dff2b38928f11d4b52397c082dd5bf99434b682'
     ChecksumType64 = 'sha256'
     UnzipLocation  = $toolsDir
 }
