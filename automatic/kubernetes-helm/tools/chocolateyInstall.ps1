ac4d9bb03f1d043e05fc208b6162bdadfcddb295
synax <stefan.henseler@synax.ch>
2019-03-06 11:34:12 +0000

    AU: 1 updated - kubernetes-helm [skip ci]

--- automatic/kubernetes-helm/tools/chocolateyInstall.ps1
+++ automatic/kubernetes-helm/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $tempDir = "$toolsDir\temp"
 
 $packageArgs = @{
     PackageName    = $packageName
-    Url64bit       = 'https://storage.googleapis.com/kubernetes-helm/helm-v2.12.3-windows-amd64.zip'
-    Checksum64     = 'c01a9e7ad27d5dded85511f244201a4821fe7157f1edbf5b601cff78c55d06a2'
+    Url64bit       = 'https://storage.googleapis.com/kubernetes-helm/helm-v2.13.0-windows-amd64.zip'
+    Checksum64     = '63fdb71ad6fac0572a21ad81da7508b1f0cae960ea944670f4d2f7fbaf23acb2'
     ChecksumType64 = 'sha256'
     UnzipLocation  = $toolsDir
 }
