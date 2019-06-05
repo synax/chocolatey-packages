3d443daa1c307af4b14b3baf470931f2f74060f0
synax <stefan.henseler@synax.ch>
2019-06-05 21:16:23 +0000

    AU: 1 updated - aks-engine [skip ci]

--- automatic/aks-engine/tools/chocolateyInstall.ps1
+++ automatic/aks-engine/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $tempDir = "$toolsDir\temp"
 
 $packageArgs = @{
     PackageName    = $packageName
-    Url64bit       = 'https://github.com/Azure/aks-engine/releases/download/v0.36.3/aks-engine-v0.36.3-windows-amd64.zip'
-    Checksum64     = '21049dae13d58316bfc05927cad38133516d558f90c1df40b2173fe3372af325'
+    Url64bit       = 'https://github.com/Azure/aks-engine/releases/download/v0.36.4/aks-engine-v0.36.4-windows-amd64.zip'
+    Checksum64     = '040a593c8e019d2ecabbce436a49385304c110fbcd8e2ad121fafe752ceb42fc'
     ChecksumType64 = 'sha256'
     UnzipLocation  = $toolsDir
 }
