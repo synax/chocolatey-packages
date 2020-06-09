32f6898a21a4a518eaa9a7c31cb2374896872d20
synax <stefan.henseler@synax.ch>
2020-06-09 00:58:00 +0000

    AU: 1 updated - kubernetes-helm [skip ci]

--- automatic/kubernetes-helm/tools/chocolateyInstall.ps1
+++ automatic/kubernetes-helm/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $tempDir = "$toolsDir\temp"
 
 $packageArgs = @{
     PackageName    = $packageName
-    Url64bit       = 'https://get.helm.sh/helm-v3.2.2-windows-amd64.zip'
-    Checksum64     = '8b28feea08d6b480419b088ba38242ce2dbc23e9a880d012ab32371e256fdd00'
+    Url64bit       = 'https://get.helm.sh/helm-v3.2.3-windows-amd64.zip'
+    Checksum64     = 'c8c4b1ed6d96faa7ed45066ad4fef073ad1be49375dfad61e036928b09a22e06'
     ChecksumType64 = 'sha256'
     UnzipLocation  = $toolsDir
 }
