478a43a334320ef0b897a18e78b1a914254631f7
synax <stefan.henseler@synax.ch>
2020-09-01 22:52:06 +0000

    AU: 2 updated - aks-engine kubernetes-helm [skip ci]

--- automatic/aks-engine/tools/chocolateyInstall.ps1
+++ automatic/aks-engine/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $tempDir = "$toolsDir\temp"
 
 $packageArgs = @{
     PackageName    = $packageName
-    Url64bit       = 'https://github.com/Azure/aks-engine/releases/download/v0.55.0/aks-engine-v0.55.0-windows-amd64.zip'
-    Checksum64     = '2bd3b40c87c73c98deb04fd81b7680241f25ad10ef0a5e2e2000f58adc465ffd'
+    Url64bit       = 'https://github.com/Azure/aks-engine/releases/download/v0.55.1/aks-engine-v0.55.1-windows-amd64.zip'
+    Checksum64     = '2619b53b8f2fe28c24ae15ec61c683c2daf79f959b3c0bef37ad41d872869324'
     ChecksumType64 = 'sha256'
     UnzipLocation  = $toolsDir
 }
--- automatic/kubernetes-helm/tools/chocolateyInstall.ps1
+++ automatic/kubernetes-helm/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $tempDir = "$toolsDir\temp"
 
 $packageArgs = @{
     PackageName    = $packageName
-    Url64bit       = 'https://get.helm.sh/helm-v3.3.0-windows-amd64.zip'
-    Checksum64     = '1bac19768e853ada10b9ee7896678a5a7352cc06546c5ea3d47652fcea1033c3'
+    Url64bit       = 'https://get.helm.sh/helm-v3.3.1-windows-amd64.zip'
+    Checksum64     = 'ee175565a81d1288769280b00ff1b78a77b225001471d0dd76117f909e707447'
     ChecksumType64 = 'sha256'
     UnzipLocation  = $toolsDir
 }
